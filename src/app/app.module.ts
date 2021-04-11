87462d5a92021f42e9bc53c86836a3b5af2b0ae2
Janchorizo <jancho@usal.es>
2021-04-11 13:42:58 +0200

    Update app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { appReducer } from '../redux/appReducer';
 import { CDFActions } from '../redux/cdfActions';
 import { ExamsActions } from '../redux/examsActions';
-import { createStore } from 'redux'
+import { createStore } from 'redux';
+import { ExamsComponent } from './exams/exams.component';
+import { LiquidContainerComponent } from './liquid-container/liquid-container.component'
 
 const store = createStore(appReducer);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamsComponent,
+    LiquidContainerComponent
   ],
   imports: [
     BrowserModule
