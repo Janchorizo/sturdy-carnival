02bda5dbac07c403d100191efac2efa7a182ef95
Janchorizo <jancho@usal.es>
2021-04-11 11:13:39 +0200

    Implement app reducer.

---
+++ src/redux/appReducer.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+import { examsReducer } from './examsReducer';
+import { cdfReducer } from './cdfReducer';
+
+
+export const appReducer = combineReducers({
+    exams: examsReducer,
+    cdf: cdfReducer,
+});
