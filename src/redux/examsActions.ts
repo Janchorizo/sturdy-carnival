a36c3ff7c19a7256ce015ad6fe211e368600fa41
Janchorizo <jancho@usal.es>
2021-04-11 10:59:16 +0200

    Change action spelling.

--- src/redux/examsActions.ts
+++ src/redux/examsActions.ts
@@ -4,7 +4,7 @@ import { ReduxAction, createActionGenerator } from './types';
 // Action types
 export const ADD_RESULT_ACTION = 'ADD_RESULT_ACTION';
 export const REMOVE_RESULT_ACTION = 'REMOVE_RESULT_ACTION';
-export const CLEAR_RESULT_ACTION = 'CLEAR_RESULT_ACTION';
+export const CLEAR_RESULTS_ACTION = 'CLEAR_RESULTS_ACTION';
 
 // Action generators
 export const addResult: (payload: any, error: boolean)=>ReduxAction =
@@ -14,5 +14,4 @@ export const removeResult: (payload: any, error: boolean)=>ReduxAction =
     createActionGenerator(ADD_RESULT_ACTION);
 
 export const clear: (payload: any, error: boolean)=>ReduxAction =
-    createActionGenerator(CLEAR_RESULT_ACTION);
-
+    createActionGenerator(CLEAR_RESULTS_ACTION);
