f5d7d2410a231b25399095dbf7faccf12ac60a03
Janchorizo <jancho@usal.es>
2021-04-11 13:42:35 +0200

    Add liquid container component.

---
+++ src/app/liquid-container/liquid-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liquid-container',
+  templateUrl: './liquid-container.component.html',
+  styleUrls: ['./liquid-container.component.css']
+})
+export class LiquidContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
